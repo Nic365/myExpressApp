a3a93be81bd763bc524461b5cb7e7dcdaede009e
Nicolas Payre <nicolas.payre@cegepsherbrooke.qc.ca>
2022-09-27 13:46:07 -0400

    another commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nico' });
+  res.render('index', { title: 'My_Web_Page', name: 'Nico' });
 });
 
 module.exports = router;
